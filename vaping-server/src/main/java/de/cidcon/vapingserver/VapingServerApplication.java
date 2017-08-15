0212888f9e2c31cb05d607da2bad9b559bddb4e1
mgrube <matthias@gizzi.de>
2017-08-15 09:33:46 +0200

    initial project import

---
+++ vaping-server/src/main/java/de/cidcon/vapingserver/VapingServerApplication.java
@@ -0,0 +1,12 @@
+package de.cidcon.vapingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VapingServerApplication.class, args);
+	}
+}
